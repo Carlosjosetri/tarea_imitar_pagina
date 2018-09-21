00f90bff630fa712e3234d379230e58253a1c4b3
carlosjosetri <00082217@uca.edu.sv>
2018-09-21 02:56:40 -0600

    gwef

---
+++ Ejercicio_4.js
@@ -0,0 +1,4 @@
+let binario =function (i)
+{
+   return (i<1)?"":((i-(i%2))/2)+i%2;
+}
---
+++ Ejercicio_5.js
@@ -0,0 +1,9 @@
+let circunferencia =function (i)
+{
+   if(i>0){
+       console.log("area es "+(3.1416*i*i));
+   }
+   else{
+       console.log("el radio es negativo o cero");
+   }
+}
